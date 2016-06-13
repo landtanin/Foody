87f75e9b5fe4bd8f55c8e7d91dcdbd6dadf988c8
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-06-13 16:27:26 +0700

    Done 2 buttons in MainAc, cancel button in signup, and toolbar in signup

--- app/src/main/java/com/appdever/foody/MainActivity.java
+++ app/src/main/java/com/appdever/foody/MainActivity.java
@@ -1,13 +1,58 @@
 package com.appdever.foody;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+
+        controlActivity();
+
+    }
+
+    private void controlActivity() {
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent objIntent = new Intent(MainActivity.this, signupActivity.class);
+//                objIntent.putExtra("user", pilotName);
+                startActivity(objIntent);
+                finish();
+
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent objIntent = new Intent(MainActivity.this, Login2Activity.class);
+//                objIntent.putExtra("user", pilotName);
+                startActivity(objIntent);
+                finish();
+
+            }
+        });
+
+    }
+
+    private void bindWidget() {
+
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
     }
 }
--- app/src/main/java/com/appdever/foody/signupActivity.java
+++ app/src/main/java/com/appdever/foody/signupActivity.java
@@ -1,13 +1,47 @@
 package com.appdever.foody;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class signupActivity extends AppCompatActivity {
+
+    private Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        bindWidget();
+
+        controlActivity();
+
+//        setTitle("                            สมัครสมาชิก");
+
+//        getSupportActionBar()
+
+    }
+
+    private void controlActivity() {
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent objIntent = new Intent(signupActivity.this, MainActivity.class);
+//                objIntent.putExtra("user", pilotName);
+                startActivity(objIntent);
+//                finish();
+
+            }
+        });
+    }
+
+    private void bindWidget() {
+
+        cancelButton = (Button) findViewById(R.id.cancelButton);
     }
 }
