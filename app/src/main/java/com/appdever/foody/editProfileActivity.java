be36f2776974baf5a9e95666b8608b15ccdb80ef
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-06-14 16:57:34 +0700

    fixed addpicture2

--- app/src/main/java/com/appdever/foody/editProfileActivity.java
+++ app/src/main/java/com/appdever/foody/editProfileActivity.java
@@ -1,13 +1,44 @@
 package com.appdever.foody;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class editProfileActivity extends AppCompatActivity {
+
+    private ImageButton addImageButton;
+    private EditText oldPassTextField, newPassTextField, verifyNewPassTextField;
+    private Button cancelButton, okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        bindWidget();
+
+        buttonController();
+
+    }
+
+    private void buttonController() {
+
+
+
+    }
+
+    private void bindWidget() {
+
+        addImageButton = (ImageButton) findViewById(R.id.addImageButton);
+
+        oldPassTextField = (EditText) findViewById(R.id.oldPassTextField);
+        newPassTextField = (EditText) findViewById(R.id.newPassTextField);
+        verifyNewPassTextField = (EditText) findViewById(R.id.verifyNewPassTextField);
+
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        okButton = (Button) findViewById(R.id.okButton);
+
     }
 }
