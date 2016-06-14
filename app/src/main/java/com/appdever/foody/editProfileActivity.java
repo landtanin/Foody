293ea3cbc8ac6414699b5a1454b9efb37fcfc406
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-06-14 17:28:48 +0700

    done EditText and button in editProfileActivity

--- app/src/main/java/com/appdever/foody/editProfileActivity.java
+++ app/src/main/java/com/appdever/foody/editProfileActivity.java
@@ -1,10 +1,13 @@
 package com.appdever.foody;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class editProfileActivity extends AppCompatActivity {
 
@@ -25,8 +28,41 @@ public class editProfileActivity extends AppCompatActivity {
 
     private void buttonController() {
 
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent objIntent = new Intent(editProfileActivity.this, MainActivity.class);
+//                objIntent.putExtra("user", pilotName);
+                startActivity(objIntent);
+//                finish();
+            }
+        });
 
 
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strNewPass = oldPassTextField.getText().toString().trim();
+                String strOldPass = newPassTextField.getText().toString().trim();
+                String strVerifyPass = verifyNewPassTextField.getText().toString().trim();
+
+                if (strNewPass.equals("") || strOldPass.equals("") || strVerifyPass.equals("")) {
+
+                    Toast.makeText(editProfileActivity.this,"กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 
     private void bindWidget() {
