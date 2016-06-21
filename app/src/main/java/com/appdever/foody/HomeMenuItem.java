bb98fd97314e79574f5e2a924d49d59d57dc6dda
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-06-21 19:00:01 +0700

    Land attempt to use GridView AddMenuFragment (missing files added)

---
+++ app/src/main/java/com/appdever/foody/HomeMenuItem.java
@@ -0,0 +1,18 @@
+package com.appdever.foody;
+
+/**
+ * Created by landtanin on 6/21/2016 AD.
+ */
+public class HomeMenuItem {
+
+    public String menuName;
+    public int image;
+
+    public HomeMenuItem(String menuName, int image) {
+
+        this.image = image;
+        this.menuName = menuName;
+
+    }
+
+}
