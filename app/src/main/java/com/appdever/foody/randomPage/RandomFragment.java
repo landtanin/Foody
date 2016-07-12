56b30956367c6c95085c166eae744c7aeb7e0573
iextreme07 <564607030007@dpu.ac.th>
2016-07-12 12:33:01 +0700

    random

--- app/src/main/java/com/appdever/foody/randomPage/RandomFragment.java
+++ app/src/main/java/com/appdever/foody/randomPage/RandomFragment.java
@@ -1,16 +1,48 @@
 package com.appdever.foody.randomPage;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.appdever.foody.MainActivity;
 import com.appdever.foody.R;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,13 +62,16 @@ public class RandomFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private ImageButton button;
+    private Bitmap bitmap;
+    private ProgressDialog pDialog;
+    private ImageView img;
+    String strFoodPic = "0";
 
     private OnFragmentInteractionListener mListener;
 
     public RandomFragment() {
         // Required empty public constructor
     }
-
 
     /**
      * Use this factory method to create a new instance of
@@ -58,6 +93,10 @@ public class RandomFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -80,9 +119,14 @@ public class RandomFragment extends Fragment {
             @Override
             public void onClick(View arg0) {
 
-                Toast.makeText(getContext(),
-                        "Button is clicked", Toast.LENGTH_LONG).show();
+                Random();
 
+               /* new LoadImage().execute(strFoodPic);*/
+
+
+                /*Toast.makeText(getContext(),
+                        "Button is clicked", Toast.LENGTH_LONG).show();
+*/
             }
         });
         return rootView;
@@ -126,4 +170,127 @@ public class RandomFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+    public boolean Random()
+    {
+
+        final AlertDialog.Builder ad = new AlertDialog.Builder(getContext());
+
+        String url = "http://foodyth.azurewebsites.net/foody/random3.php";
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        String resultServer  = getHttpPost(url,params);
+
+        /*** Default Value ***/
+        String strStatusID = "0";
+        String strFoodID = "0";
+        String strFoodName = "0";
+
+        String strError = "Unknow Status!";
+
+        JSONObject c;
+        try {
+            c = new JSONObject(resultServer);
+            strStatusID = c.getString("StatusID");
+            strFoodID = c.getString("id_food");
+            strFoodName = c.getString("name_food");
+            /*strFoodPic = c.getString("FoodPic");*/
+            strError = c.getString("Error");
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // Prepare Login
+        if(strStatusID.equals("1"))
+        {
+
+            // Dialog
+            ad.setTitle("อาหารที่ถูกสุ่ม");
+            //      ad.setIcon(android.R.drawable.btn_star_big_on);
+            ad.setPositiveButton("Close", null);
+            ad.setMessage(strFoodID);
+            ad.setMessage(strFoodName);
+
+            ad.show();
+        }
+
+      /*  else
+        {
+            Toast.makeText(getContext(), "Login OK", Toast.LENGTH_SHORT).show();
+            Intent newActivity = new Intent(getActivity(),MainActivity.class);
+            newActivity.putExtra("FoodID","FoodName");
+            startActivity(newActivity);
+        }*/
+        return true;
+    }
+
+
+    public String getHttpPost(String url,List<NameValuePair> params) {
+        StringBuilder str = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) { // Status OK
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                }
+            } else {
+                Log.e("Log", "Failed to download result..");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(getContext());
+            pDialog.setMessage("Loading Image ....");
+            pDialog.show();
+
+        }
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap image) {
+
+            if(image != null){
+                img.setImageBitmap(image);
+                pDialog.dismiss();
+
+            }else{
+
+                pDialog.dismiss();
+         /*       Toast.makeText(MainActivity.this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();*/
+
+            }
+        }
+    }
+
+
 }
