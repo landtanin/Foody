47ee2ef932b56a2b6c8ea5a6b81735da0c646d7d
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-07-13 17:11:52 +0700

    populate foodName obtain from JSON to recyclerView in EnterSearchActivity

--- app/src/main/java/com/appdever/foody/searchPage/enterSearch/EnterSearchActivity.java
+++ app/src/main/java/com/appdever/foody/searchPage/enterSearch/EnterSearchActivity.java
@@ -3,6 +3,7 @@ package com.appdever.foody.searchPage.enterSearch;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -20,12 +21,14 @@ import com.appdever.foody.KeyStore;
 import com.appdever.foody.R;
 import com.appdever.foody.databinding.ActivityEnterSearchBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.Response;
 
@@ -37,12 +40,22 @@ public class EnterSearchActivity extends AppCompatActivity implements AdapterVie
     private EnterSearchRecyclerAdapter recyclerAdapter;
     List<EnterSearchMenu> newsList = new ArrayList<>();
 
+    private String resultServer;
+    private String strFoodID = "0";
+    private String strFoodTypeID = "0";
+    private String strNameFood;
+    private String strCookingMethod = "Unknown method";
+    private String strImg = "Unknown image";
+    private String strError = "Unknown Status!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_enter_search);
 
         binding.enterSearchSpinner.setOnItemSelectedListener(this);
+
+        connectDatabase();
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.hard_code_menus, android.R.layout.simple_spinner_item);
@@ -69,12 +82,10 @@ public class EnterSearchActivity extends AppCompatActivity implements AdapterVie
         rv.setAdapter(recyclerAdapter);
 
         rv.setHasFixedSize(true);
-        for (int i=0; i < 20; i++){
-            newsList.add(new EnterSearchMenu("Test"));
-        }
+//        for (int i=0; i < 1; i++){
+//
+//        }
 
-
-        connectDatabase();
 
     }
 
@@ -84,22 +95,125 @@ public class EnterSearchActivity extends AppCompatActivity implements AdapterVie
 // optional
         int getSendKey = getIntent().getExtras().getInt(KeyStore.SELECT_FOOD_SEND_KEY);
 
-        HttpUrl myurl = HttpUrl.parse(JSONObtained.getAbsoluteUrl("food.php")).newBuilder().addQueryParameter("id_typefood", String.valueOf(getSendKey)).build();
+        final HttpUrl myurl = HttpUrl.parse(JSONObtained.getAbsoluteUrl("food.php")).newBuilder().addQueryParameter("id_typefood", String.valueOf(getSendKey)).build();
 
-        JSONObtained.getInstance().newCall(JSONObtained.getRequest(myurl)).enqueue(new Callback() {
-
+        new AsyncTask<Void, Void, String>() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d("eeeeee", e.getLocalizedMessage());
+            protected String doInBackground(Void... params) {
+                try {
+
+                    Response response = JSONObtained.getInstance().newCall(JSONObtained.getRequest(myurl)).execute();
+
+                    if (response.isSuccessful()) {
+
+                        resultServer = response.body().string();
+
+                        Log.d("xxxxxx", resultServer);
+
+                        JSONObject foodJSONStr, ID_food = null;
+
+                        try {
+                            foodJSONStr = new JSONObject(resultServer);
+                            JSONArray foods = foodJSONStr.getJSONArray("foods");
+
+
+                            for (int i = 0; i<foods.length(); i++) {
+
+                                ID_food = foods.getJSONObject(i);
+                                strFoodID = ID_food.getString("id_food");
+                                strFoodTypeID = ID_food.getString("id_typefood");
+                                strNameFood = ID_food.getString("name_food");
+                                strCookingMethod = ID_food.getString("cooking_method");
+                                strImg = ID_food.getString("img");
+
+                                newsList.add(new EnterSearchMenu(strNameFood));
+
+                            }
+
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.d("xxxxxx", response.body().string());
+            protected void onPostExecute(String s) {
+
+                recyclerAdapter.notifyDataSetChanged();
+
+                super.onPostExecute(s);
             }
-        });
 
 
+        }.execute();
+
+
+
+
+
+//        JSONObtained.getInstance().newCall(JSONObtained.getRequest(myurl)).enqueue(new Callback() {
+
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                Log.d("eeeeee", e.getLocalizedMessage());
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//
+//                resultServer = response.body().string();
+//
+//                Log.d("xxxxxx", resultServer);
+//
+//                JSONObject foodJSONStr, ID_food = null;
+//
+//                try {
+//                    foodJSONStr = new JSONObject(resultServer);
+//                    JSONArray foods = foodJSONStr.getJSONArray("foods");
+//
+//
+//                    for (int i = 0; i<foods.length(); i++) {
+//
+//                        ID_food = foods.getJSONObject(i);
+//                        strFoodID = ID_food.getString("id_food");
+//                        strFoodTypeID = ID_food.getString("id_typefood");
+//                        strNameFood = ID_food.getString("name_food");
+//                        strCookingMethod = ID_food.getString("cooking_method");
+//                        strImg = ID_food.getString("img");
+//
+//                        newsList.add(new EnterSearchMenu(strNameFood));
+//
+//                    }
+//
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            recyclerAdapter.notifyDataSetChanged();
+//                        }
+//                    });
+//
+//
+//
+//                    Log.d("JSONObjectArray", String.valueOf(ID_food));
+//
+////            strFoodID = c.getString("id_food");
+////            strFoodName = c.getString("name_food");
+////            /*strFoodPic = c.getString("FoodPic");*/
+////            strError = c.getString("Error");
+//
+//                } catch (JSONException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//        });
 
     }
 
@@ -113,11 +227,12 @@ public class EnterSearchActivity extends AppCompatActivity implements AdapterVie
 
         Toast.makeText(EnterSearchActivity.this,item, Toast.LENGTH_SHORT).show();
 
-
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
+
