7cb22b91e7c48eed963157ce653c7924d8559390
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-07-15 17:31:55 +0700

    Update FoodDetail again

--- app/src/main/java/com/appdever/foody/FoodDetailActivity.java
+++ app/src/main/java/com/appdever/foody/FoodDetailActivity.java
@@ -1,18 +1,13 @@
 package com.appdever.foody;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class FoodDetailActivity extends AppCompatActivity {
-
-    private String strFoodID, strFoodTypeID, strNameFood, strCookingMethod, strImg,strFoodIngredient, strFoodDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
-
-
-
     }
 }
