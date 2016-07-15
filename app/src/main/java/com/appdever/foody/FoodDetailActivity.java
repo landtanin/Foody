4790990a27dcebf9d70dee4efa0be5d815ca9bae
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-07-15 17:23:19 +0700

    Update FoodDetail

--- app/src/main/java/com/appdever/foody/FoodDetailActivity.java
+++ app/src/main/java/com/appdever/foody/FoodDetailActivity.java
@@ -1,11 +1,7 @@
 package com.appdever.foody;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.appdever.foody.databinding.ActivityFoodDetailBinding;
-import com.bumptech.glide.Glide;
 
 public class FoodDetailActivity extends AppCompatActivity {
 
@@ -14,21 +10,9 @@ public class FoodDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityFoodDetailBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_food_detail);
+        setContentView(R.layout.activity_food_detail);
 
-        Bundle extras = getIntent().getExtras();
-        strFoodID = extras.getString(KeyStore.FOODID_DETAIL_SEND_KEY);
-        strFoodTypeID = extras.getString(KeyStore.FOODTYOEID_DETAIL_SEND_KEY);
-        strNameFood = extras.getString(KeyStore.NAMEFOOD_DETAIL_SEND_KEY);
-        strCookingMethod = extras.getString(KeyStore.FOODMETHOD_DETAIL_SEND_KEY);
-        strImg = extras.getString(KeyStore.FOODIMG_DETAIL_SEND_KEY);
-        strFoodIngredient = extras.getString(KeyStore.FOOD_INGREDIENT_SEND_KEY);
-        strFoodDescription = extras.getString(KeyStore.FOOD_DESCRIPTION_SEND_KEY);
 
-        Glide.with(this).load(strImg).into(binding.detailImg);
-        binding.detailFoodNameTxt.setText(strNameFood);
-        binding.materialDetailText.setText(strFoodIngredient);
-        binding.methodDetailText.setText(strCookingMethod);
 
     }
 }
